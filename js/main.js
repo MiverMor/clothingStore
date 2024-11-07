450f6ab3267230951f891746a4e1e36031cb947f
MiverMor <ivan.mordyashev@mail.ru>
2024-11-07 15:04:34 +0800

    catalog & card page finished

--- js/main.js
+++ js/main.js
@@ -42,3 +42,119 @@ var discountsSlider = new Swiper(".discounts-slider", {
     prevEl: '.discounts-slider__button--prev',
   },
 });
+
+const productSlider = new Swiper('.card-product__slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.card-product__button--next',
+    prevEl: '.card-product__button--prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.card-product__slider--scrollbar',
+  },
+});
+
+$(function(){
+  $(document).on("click",'.sort__label',function(e){
+		$(this).closest(".sort").toggleClass("_active");
+  });
+
+
+$(document).on("click",".catalog__switcher",function(e){
+  let sidebar = $(".catalog__sidebar");
+
+  if (!sidebar.hasClass("show")) {
+    sidebar.addClass("show");
+  } else {
+    sidebar.removeClass("show");
+  }
+});
+
+$('.product__price').each(function(){
+		let priceBlock = $(this);
+		let discount = priceBlock.attr('product-discount');
+		if (discount) {
+        // Убираем символ процента и преобразуем скидку в число
+        let discountValue = parseFloat(discount.replace('%', ''));
+        
+        // Получаем старую цену из тега <b>
+        let oldPriceText = priceBlock.find('b').text().replace(/\s/g, '').replace('RUB', '');
+        let oldPrice = parseFloat(oldPriceText);
+        
+        // Вычисляем новую цену
+        let newPrice = oldPrice - (oldPrice * (discountValue / 100));
+        
+        // Оборачиваем старую цену в тег <s>
+        let oldPriceHtml = '<s>' + oldPrice.toLocaleString() + ' RUB</s>';
+        
+        // Добавляем новую цену в тег <b>
+        let newPriceHtml = '<b>' + newPrice.toLocaleString() + ' RUB</b>';
+        
+        // Обновляем HTML внутри priceBlock
+        priceBlock.html(oldPriceHtml + ' ' + newPriceHtml);
+
+        // Добавляем элемент со скидкой, если он еще не существует
+        let pic = $(this).closest(".product__link");
+        if (!pic.find('.product__discount').length) {
+            pic.append('<div class="product__discount"><span>' + discount + '</span></div>');
+        }
+    }
+});
+
+$(document).on("click", ".skuprops__btn", function(e) {
+  let skupropsSel = $(this); // текущий элемент, на который кликнули
+
+  // Удаляем класс 'active' у всех кнопок
+  $(".skuprops__btn").removeClass("skuprops__values--active");
+
+  // Добавляем класс 'active' только выбранному элементу
+  skupropsSel.addClass("skuprops__values--active");
+});
+
+$(document).on("click", ".color-select__btn", function(e) {
+  let colorSel = $(this); // текущий элемент, на который кликнули
+
+  // Удаляем класс 'active' у всех кнопок
+  $(".color-select__btn").removeClass("color-select__btn--active");
+
+  // Добавляем класс 'active' только выбранному элементу
+  colorSel.addClass("color-select__btn--active");
+});
+
+
+$('.product__price').each(function(){
+		let priceBlock = $(this);
+		let discount = priceBlock.attr('product-discount');
+		if (discount) {
+        // Убираем символ процента и преобразуем скидку в число
+        let discountValue = parseFloat(discount.replace('%', ''));
+        
+        // Получаем старую цену из тега <b>
+        let oldPriceText = priceBlock.find('b').text().replace(/\s/g, '').replace('RUB', '');
+        let oldPrice = parseFloat(oldPriceText);
+        
+        // Вычисляем новую цену
+        let newPrice = oldPrice - (oldPrice * (discountValue / 100));
+        
+        // Оборачиваем старую цену в тег <s>
+        let oldPriceHtml = '<s>' + oldPrice.toLocaleString() + ' RUB</s>';
+        
+        // Добавляем новую цену в тег <b>
+        let newPriceHtml = '<b>' + newPrice.toLocaleString() + ' RUB</b>';
+        
+        // Обновляем HTML внутри priceBlock
+        priceBlock.html(oldPriceHtml + ' ' + newPriceHtml);
+
+        // Добавляем элемент со скидкой, если он еще не существует
+        let title = $(this).closest('.card-product__details').find('.card-product__title'); // Ищем заголовок
+        if (!title.find('.card-product__discount').length) {
+            title.append('<div class="card-product__discount"><span>' + discount + '</span></div>');
+        }
+    }
+});
+});
